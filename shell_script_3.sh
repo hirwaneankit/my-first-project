8f56b918361154db20141263916ac788831a9b39
hirwaneankit <hirwaneankit@gmail.com>
2024-03-20 15:05:51 +0530

    update date 20-3-2023

---
+++ shell_script_3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -n "Enter the value of first variable1= "
+read V1
+
+echo -n "Enter the Value of Second Variable2= "
+read V2
+
+echo "Variable1 value = $V1"
+echo "Variable2 value = $V2"
+
